30ab93f4f4629fc74d6b45ee7f499f5b71980fd6
Santiago <santiago.bocanegra@hotmail.com>
2022-08-17 10:49:06 -0500

    link .js file to .html file and write pseudocode

---
+++ game.js
@@ -0,0 +1,10 @@
+get computer choice randome between rock paper scissors
+get user choice rock paper or scissors
+assign values to rock paper and scissors
+rock is greater than scissors and less than paper
+paper is greater than rock and less than scissors
+scissors are greater than paper and less than rock
+compare imput and computer choice
+user choice is greater you won 
+computer is greater you lost 
+start again
