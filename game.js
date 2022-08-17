5974e1cbb9a3c29a93adea1ac543e4a5d1dd9a95
Santiago <santiago.bocanegra@hotmail.com>
2022-08-17 14:16:43 -0500

    succesfully get compChoice

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-get computer choice randome between rock paper scissors
+/*get computer choice randome between rock paper scissors
 get user choice rock paper or scissors
 assign values to rock paper and scissors
 rock is greater than scissors and less than paper
@@ -7,4 +7,33 @@ scissors are greater than paper and less than rock
 compare imput and computer choice
 user choice is greater you won 
 computer is greater you lost 
-start again
+start again */
+/*let userChoice = prompt("Rock, Paper or Scissors", "Choose");
+userChoice = userChoice.toLowerCase();
+let compChoice = getComputerChoice();
+function getComputerChoice() {
+    let randomnumber = Math.floor(Math.random(3)*3);
+    if (randomnumber === 0) {
+        return "Rock";
+    }
+    else if (randomnumber === 1) {
+        return "Paper";
+    }
+    else if (randomnumber === 2) {
+        return "Scissors";
+    }
+}
+second attempt try to get a working result from random number*/
+let randomNumber = Math.floor(Math.random()*3);
+    if (randomNumber === 0) {
+        compChoice = "Rock";
+    }
+    else if (randomNumber === 1) {
+        compChoice = "Paper";
+    }
+    else if (randomNumber === 2){
+        compChoice = "Scissors";
+    }
+console.log(compChoice);
+//So far works to get a random number 0,1,2
+
