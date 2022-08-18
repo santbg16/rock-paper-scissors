f3662be5fb96694c341e3262c653ffbf8282e148
Santiago <santiago.bocanegra@hotmail.com>
2022-08-18 13:47:25 -0500

    plays game displays score

--- game.js
+++ game.js
@@ -34,11 +34,12 @@ let randomNumber = Math.floor(Math.random()*3);
     else if (randomNumber === 2){
         compChoice = "scissors";
     }
-//console.log(compChoice);
+console.log(compChoice);
 //So far works to get a random number 0,1,2
 // By getting compChoice managed to get rock, paper or scissors
 let userChoice = prompt("Rock, Paper or Scissors?","");
 userChoice = userChoice.toLowerCase();
+console.log(userChoice)
 //console.log(userChoice);
 //Prompt user to imput choice and changed to lower case
 let winner = (compChoice , userChoice)
@@ -54,8 +55,47 @@ let winner = (compChoice , userChoice)
     else if (compChoice === "scissors" && userChoice === "rock") {
         result = "You win!";
     }
+    else if (userChoice === null || ""){
+        result = "Try imput again";
+    } //previous condition does not work
     else {
         result = "You loose :(";
     }
 console.log(result)
+
 //finally managed to play. Works till here. 
+
+/*Now needs to store result. Keep tally, if "You win!" user +1 to score. 
+if "You loose :(" computer + 1 score
+else prompt again and tallies remain the same.
+if user or comp reach 5 display won or lost match.
+use loop to determine number of games played
+reset and try again*/
+//second attempt at using a declared function.
+/*let compChoice = randomChoice ();
+    function randomChoice () {
+    let randomNumber= Math.floor(Math.random()*3);
+    if (randomNumber === 0) {
+        return "rock";
+    }
+    else if (randomNumber === 1) {
+        return "paper";
+    }
+    else if (randomNumber === 2){
+        return "scissors";
+    }
+} console.log (compChoice)*/
+let userScore = 0
+let compScore = 0
+  if (result === "You win!") {
+    userScore++
+  } 
+  else if (result === "You loose :(") {
+    compScore++
+  }
+  console.log(userScore,compScore)
+  //displays score need to loop game to play other rounds and keep score.
+  let displayScore = function(){
+    return "Your score: " + userScore + " Computer score: " + compScore
+  }
+  console.log(displayScore())
